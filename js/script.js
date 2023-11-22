13656fa2b38d5e10a53a4d0bf96b908db0291bbe
Ion <bragaion99@gmail.com>
2023-11-22 23:48:36 +0200

    Commit 2

--- js/script.js
+++ js/script.js
@@ -9,12 +9,28 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('unul din filmele privite?', ''),
-      b = prompt('Ce nota ii dati?', ''),
-      c = prompt('unul din filmele privite?', ''),
-      d = prompt('Ce nota ii dati?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('unul din filmele privite?', ''),
+          b = prompt('Ce nota ii dati?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Destul de putine filme');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Utilizator clasic');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Esti kinoman');
+} else {
+    console.log('error');
+}
 
 console.log(personalMovieDB);
